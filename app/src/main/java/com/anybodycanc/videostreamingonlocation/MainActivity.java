7db2e31d58797dc28e4bc5f78f07933836c0961d
Sid <sid@Sids-MacBook-Pro.local>
2017-05-13 13:45:32 +0530

    Initial commit

---
+++ app/src/main/java/com/anybodycanc/videostreamingonlocation/MainActivity.java
@@ -0,0 +1,161 @@
+package com.anybodycanc.videostreamingonlocation;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnBufferingUpdateListener;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.media.MediaPlayer.OnVideoSizeChangedListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.MediaController;
+import android.widget.MediaController.MediaPlayerControl;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, OnPreparedListener,
+        OnBufferingUpdateListener, OnCompletionListener,
+        OnVideoSizeChangedListener, MediaPlayerControl {
+
+    private MediaPlayer mediaPlayer;
+    private MediaController mediaController;
+    private Handler handler = new Handler();
+    private SurfaceHolder vidHolder;
+    private SurfaceView vidSurface;
+    String vidAddress = "http://anybodycanc.com/files/test_me.mp4";
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        /*
+         * the MediaController will hide after 3 seconds - tap the screen to
+         * make it appear again
+         */
+        mediaController.show();
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView vidView = (VideoView)findViewById(R.id.myVideo);
+//        String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+        Uri vidUri = Uri.parse(vidAddress);
+        vidView.setVideoURI(vidUri);
+        MediaController vidControl = new MediaController(this);
+        vidControl.setAnchorView(vidView);
+        vidView.setMediaController(vidControl);
+        vidView.start();
+        //----
+//        vidSurface = (SurfaceView) findViewById(R.id.surfView);
+//
+//        mediaController = new MediaController(this);
+//        mediaController.setAnchorView(vidSurface);
+//        vidHolder = vidSurface.getHolder();
+//        vidHolder.addCallback(this);
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDisplay(vidHolder);
+            mediaPlayer.setDataSource(vidAddress);
+            mediaPlayer.prepare();
+            mediaPlayer.setOnPreparedListener(this);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return 0;
+    }
+
+    @Override
+    public void seekTo(int pos) {
+
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
+    }
+}
